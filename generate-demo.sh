823b201224c0aed62c26fc748dc406861e36f620
I Gede Putu Surya D P <gede.putu@bukalapak.com>
2020-11-07 21:03:14 +0700

    Update generator script

--- generate-demo.sh
+++ generate-demo.sh
@@ -2,9 +2,11 @@
 
 git checkout -b demo
 rm -rf docs
+git pull origin main
 yarn build --dest docs
 git add .
 git commit -m "build demo $(date +'%Y%m%d-%H%M%S')"
 git push origin demo
 git checkout main
 rm -rf docs
+git branch -D demo
